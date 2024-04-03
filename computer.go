406f0cab7ee3155e2c72ccecba05e1d77d30726c
notmarshmllow <60079743+notmarshmllow@users.noreply.github.com>
2024-04-03 18:32:18 +0530

    Update computer.go

--- computer.go
+++ computer.go
@@ -6,5 +6,5 @@ import "fmt"
 
 // ClaimComputer prints "Computer is mine" to the console
 func ClaimComputer() {
-    fmt.Println("This computer has been hacked!")
+    fmt.Println("Computer is mine")
 }
